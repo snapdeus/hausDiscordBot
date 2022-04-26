4ed738d9d90ee16162ef44d2a4b479d7ebf5b9f1
snapdeus <snapdeus@gmail.com>
2022-04-26 15:23:00 -0400

    added some required, test commit really

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-
+require('dotenv').config
 
 const path = require('path')
+const axios = require('axios')
+
+const { Client, Intents } = require('discord.js');
+
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
