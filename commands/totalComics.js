f80379d01c553f4e03bfaf356b028f8c3cadd64a
snapdeus <snapdeus@gmail.com>
2022-04-29 21:03:07 -0400

    added total comics command

---
+++ commands/totalComics.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const ObjectID = require('mongodb').ObjectId;
+const GayComic = require('../models/gayComics')
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'totalcomics',
+    description: 'get comic',
+
+    async execute(message, args) {
+
+
+
+
+        const totalComics = async () => {
+            try {
+                const maxNumber = await GayComic.findOne({})
+                    .sort({ ordinality: -1 })
+                const totalGayComics = maxNumber.ordinality
+
+
+
+
+                message.channel.send(`Current Total Comics: ${ totalGayComics }`);
+
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        totalComics();
+    }
+}
