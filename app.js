d3dd91b8ee12c4a49ce99800fe6ab5f677f6ebd7
snapdeus <snapdeus@gmail.com>
2022-05-18 13:42:05 -0400

    added ridiculous amount of searching for cum sum

--- app.js
+++ app.js
@@ -95,8 +95,22 @@ client.on('messageCreate', message => {
     }
     // if (message.content.toLowerCase().includes('cum sum') || (message.content.toLowerCase().includes('sum') && message.content.toLowerCase().includes('cum')) || (message.content.match(/c.m/gi) && message.content.match(/s.m/gi))) 
 
+    // console.log(message.content.normalize('NFD').replace(/[\u0300-\u036f]/g, ""))
 
-    if ((message.content.match(/\bc.m\b/gi) && message.content.match(/\bs.m\b/gi))) {
+    // if ((message.content.trim().match(/\bc.*m\b/gi) && message.content.trim().match(/\bs.*m\b/gi) || message.content.trim().replace(/\s/g, '').match('cumsum'))) {
+    //     message.delete()
+    //     message.channel.send('NO.')
+
+    // }
+
+    const cumArgs = message.content.split(' ')
+
+    const msgContent1 = Array.from(new Set(cumArgs[0])).join('')
+    const msgContent2 = Array.from(new Set(cumArgs[1])).join('')
+    // console.log(msgContent1, msgContent2)
+
+
+    if ((message.content.toLowerCase().trim().normalize('NFD').replace(/[\u0300-\u036f]/g, "").match('cum') && message.content.toLowerCase().trim().normalize('NFD').replace(/[\u0300-\u036f]/g, "").match('sum') || message.content.trim().replace(/\s/g, '').match('cumsum') || msgContent1.match('cum') && msgContent2.match('sum'))) {
         message.delete()
         message.channel.send('NO.')
 
