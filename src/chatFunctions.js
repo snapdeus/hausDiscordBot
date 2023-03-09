5da040f3315882bdd99f4895df850a60ea7568a3
snapdeus <snapdeus@gmail.com>
2023-03-08 22:01:54 -0500

    added memory

--- src/chatFunctions.js
+++ src/chatFunctions.js
@@ -11,20 +11,31 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 
+const memory = [];
+
 
 
 async function chatWithAi(args) {
+    if (memory.length > 5) {
+        memory.shift();
+    }
+    console.log(memory)
     try {
+
         const prompt = args.join(' ')
+        memory.push(prompt)
+
         const completion = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             max_tokens: 500,
             messages: [
                 { role: "system", content: "You are a helpful assistant named hausBot." },
+                { role: "assistant", content: memory.join(" ") },
                 { role: "user", content: prompt }
             ],
         });
-
+        console.log(completion.data)
+        memory.push(completion.data.choices[0].message.content)
         return completion.data.choices[0].message.content
 
     } catch (e) {
