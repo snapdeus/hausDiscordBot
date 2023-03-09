bd7c20545092477bb52eed1ea6ed7ef8e263c897
snapdeus <snapdeus@gmail.com>
2023-03-08 22:19:51 -0500

    tweaks

--- commands/chat.js
+++ commands/chat.js
@@ -13,14 +13,14 @@ module.exports = {
     async execute(client, message, args, member) {
         let role = member._roles[0]
 
-        if (args.length > 140) {
-            message.channel.send(`Please limit the length your prompt to 140 characters.`);
+        if (args.length > 200) {
+            message.channel.send(`Please limit the length your prompt to 200 characters.`);
             return
         }
 
         if (allowedRoles.includes(role)) {
             if (talkedRecently.has(message.author.id)) {
-                message.channel.send("Please wait 1 minute before issuing another prompt.")
+                message.channel.send("Please wait 30s before issuing another prompt.")
             } else {
                 try {
                     message.channel.send(`${ await chatWithAi(args, message.author.username) }`);
@@ -32,13 +32,13 @@ module.exports = {
                 setTimeout(() => {
                     // Removes the user from the set after a minute
                     talkedRecently.delete(message.author.id);
-                }, 60000);
+                }, 30000);
             }
 
 
 
         } else {
-            message.channel.send(`You do not have permission to chat with me.`);
+            message.channel.send(`You do not currently have permission to chat with me.`);
         }
     }
 }
