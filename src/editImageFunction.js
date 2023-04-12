2f8de7ea37758949df837e5f8002bf90ec182b36
snapdeus <snapdeus@gmail.com>
2023-04-11 23:08:56 -0400

    edit

---
+++ src/editImageFunction.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const User = require('../models/user')
+const { Configuration, OpenAIApi } = require("openai");
+const Discord = require('discord.js');
+
+
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+
+
+async function chatWithAi(args, message, user) {
+    const initiliazing = "Initializing..."
+    const username = message.author.username;
+    const userId = message.author.id;
+    const guildId = message.guild.id;
+
+
+    try {
+
+        const prompt = args.join(' ')
+
+
+        const completion = await openai.createImage({
+            prompt: prompt,
+            n: 1,
+            size: "256x256",
+        });
+
+
+        const chatResponse = completion.data.data[0].url
+
+
+        return chatResponse
+
+    } catch (e) {
+        console.log(`api error ${ e.response.data.error.message }`)
+        return e.response.data.error.message
+    }
+
+}
+
+module.exports = { chatWithAi }
