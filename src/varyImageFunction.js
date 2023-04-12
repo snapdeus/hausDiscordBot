5b453375268d23ca3865f15825afdc051e667570
snapdeus <snapdeus@gmail.com>
2023-04-12 14:33:03 -0400

    added image variation

--- src/varyImageFunction.js
+++ src/varyImageFunction.js
@@ -33,65 +33,30 @@ async function chatWithAi(args, message, user) {
     const userId = message.author.id;
     const guildId = message.guild.id;
 
-    const goodUrl = 'https://i.imgur.com/GLpGEhT.png'
+
 
     try {
-        console.log('trying..........')
+
         const prompt = args.join(' ')
         const url = message.attachments.first()?.url
         const mask = fs.createReadStream(path.join(dir, "mask.png"))
         await loadImageAndDraw(url)
         const canvasBuffer = canvas.toBuffer('image/png')
         canvasBuffer.name = 'image.png'
-        //below currently works
-        // const imageBuffer = await axios.get(url, { responseType: 'arraybuffer' })
-        //     .then((response) => {
-        //         const buffer = new Buffer.from(response.data)
-        //         buffer.name = 'image.png'
-        //         return buffer
-        //     })
-        //     .catch((error) => {
-        //         console.error('An error occurred while downloading the file:', error);
-        //     });
 
-        // console.log(imageBuffer)
 
-        console.log(prompt)
-        const completion = await openai.createImageEdit(
+
+
+
+        const completion = await openai.createImageVariation(
             canvasBuffer,
-            prompt,
-            fs.createReadStream(path.join(dir, "mask.png")),
             1,
             "256x256"
         );
 
 
-        //ORDER FOR NO MASK
-        // const completion = await openai.createImageEdit(
-        //     imageBuffer,
-        //     prompt,
-        //     "256x256"
-        // );
-
-        //ORDER FOR MASK
-        // const completion = await openai.createImageEdit(
-        //     imageBuffer,
-        //     prompt,
-        //     mask,
-        //     1,
-        //     "256x256"
-        // );
-
-        //VARIATION
-        // const completion = await openai.createImageVariation(
-        //     imageBuffer,
-        //     1,
-        //     "256x256"
-        // );
-
-
         const chatResponse = completion.data.data
-        console.log(chatResponse)
+
         return chatResponse
 
 
--- utiltesting.js
+++ utiltesting.js
@@ -1,13 +1,60 @@
-function splitMessage(message, maxLength = 20) {
-    const messageChunks = [];
-    for (let i = 0; i < message.length; i += maxLength) {
-        messageChunks.push(message.slice(i, i + maxLength));
-        console.log(messageChunks)
-    }
-    return messageChunks;
+
+require('dotenv').config()
+const User = require('../models/user')
+const { Configuration, OpenAIApi } = require("openai");
+const Discord = require('discord.js');
+
+const fs = require('fs')
+const path = require('path')
+const dir = path.join(__dirname)
+const PNG = require("pngjs").PNG;
+
+const { createCanvas, loadImage, Image } = require('canvas')
+
+
+const width = 256
+const height = 256
+const canvas = createCanvas(width, height);
+const ctx = canvas.getContext('2d');
+async function loadImageAndDraw(imagePath) {
+    const image = await loadImage(imagePath);
+    ctx.drawImage(image, 0, 0, 230, 230);
 }
-const latinText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-splitMessage(latinText)
+const url =
+
+    async function generateStuff() {
+        await loadImageAndDraw(url)
+        const canvasBuffer = canvas.toBuffer('image/png')
+        canvasBuffer.name = 'image.png'
 
 
+    }
+
+generateStuff();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function splitMessage(message, maxLength = 20) {
+//     const messageChunks = [];
+//     for (let i = 0; i < message.length; i += maxLength) {
+//         messageChunks.push(message.slice(i, i + maxLength));
+//         console.log(messageChunks)
+//     }
+//     return messageChunks;
+// }
+// const latinText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+// splitMessage(latinText)
