fbdcbb95e05bb0b593e0fd233bce109fbeeba763
snapdeus <snapdeus@gmail.com>
2023-04-13 15:40:36 -0400

    test again

--- cronjobs/randomComic.js
+++ cronjobs/randomComic.js
@@ -28,7 +28,7 @@ const generateRandom = async (min, max, exclude) => {
     let ranNum = Math.floor(Math.random() * (max - min)) + min;
     let cache = await db.get('comicsArray');
     if (ranNum === exclude || cache.includes(ranNum)) {
-        return generateRandom(min, max, exclude);
+        return await generateRandom(min, max, exclude);
     }
     await addComicToCache(ranNum);
     return ranNum;
