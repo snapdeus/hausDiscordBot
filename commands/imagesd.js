4abc00725f356a0d194d687045dd41cf69887c5d
snapdeus <snapdeus@gmail.com>
2023-04-19 22:39:13 -0400

    added stable diffusion image

---
+++ commands/imagesd.js
@@ -0,0 +1,115 @@
+
+const { chatWithAi } = require('../src/imageSDGenFunction')
+
+const User = require('../models/user')
+const { v4: uuidv4 } = require('uuid');
+
+
+const allowedRoles = ['973978697910599781', '974050213075513405', '973348302055682088', '954022194520932393', '954012026039070741', '973619573754564648', '954034692598951976', '953988101859049553', '973620158423781496', '968476411474112533', '972920088061694053', '1083503275447423146', '974050213075513405']
+
+
+const talkedRecently = new Set();
+
+function splitMessage(message, maxLength = 2000) {
+    const messageChunks = [];
+    for (let i = 0; i < message.length; i += maxLength) {
+        messageChunks.push(message.slice(i, i + maxLength));
+    }
+    return messageChunks;
+}
+module.exports = {
+    name: 'imagesd',
+    description: 'generate image from stable diffusion',
+
+    async execute(client, message, args, member) {
+        const username = message.author.username;
+        const userId = message.author.id;
+        const guildId = message.guild.id;
+
+        // FIND USER IN DB
+        const user = await User.findOne({ userId: userId })
+        if (!user) {
+            const user = new User({
+                username: `${ username }`,
+                userId: `${ userId }`,
+                guildId: `${ guildId }`,
+                xpOverTime: 0,
+                memories: [],
+            })
+            await user.save()
+            return "Initializing...";
+        }
+
+        // INITIALIZE MEMORY THE FIRST TIME
+        //PUT THIS IN SEEDS FILE
+        // await Memory.deleteMany({})
+        // const memory = await Memory.findOne({})
+        // if (!memory) {
+        //     const memory = new Memory({
+        //         memories: [
+        //             {
+        //                 memoryId: uuidv4(),
+        //                 userStatement: "",
+        //                 aiStatement: "",
+        //                 userId: "",
+        //                 userName: "",
+        //                 aiName: "",
+        //             }
+
+        //         ]
+        //     })
+
+        //     await memory.save()
+        //     return message.channel.send("Initializing memory...please wait 1 second and try again.");
+        // }
+
+        if (message.channel.id !== '975202962173485186' && message.channel.id !== '884434543543726134') {
+            message.channel.send("Please chat with the ai in the 'ai-chat' channel.");
+            return
+        }
+
+        let role = member._roles[0]
+
+        if (args.length > 2000) {
+            message.channel.send(`Please limit the length your prompt to 2000 characters.`);
+            return
+        }
+
+        if (user.xpOverTime > 500) {
+            if (talkedRecently.has(message.author.id)) {
+                message.channel.send("Please wait 5s before issuing another prompt.")
+            } else {
+                try {
+                    const chatBot = client.user.username;
+                    const chatResponse = await chatWithAi(args, message, user, chatBot)
+
+
+                    message.reply({
+                        content: `Here ya go⁣ ${ username }`, files: [
+                            { attachment: chatResponse }
+                        ]
+                    })
+
+                    user.xpOverTime = user.xpOverTime - 10
+                    await user.save()
+
+                } catch (e) {
+                    message.channel.send(`ERROR`);
+                    console.log(e)
+                }
+
+                talkedRecently.add(message.author.id);
+                setTimeout(() => {
+                    // Removes the user from the set after a minute
+                    talkedRecently.delete(message.author.id);
+                }, 1000);
+
+            }
+
+
+
+        } else {
+            message.channel.send(`You do not currently have permission to chat with me. Please acquire 🪙500 Haus Coins`);
+        }
+    }
+}
---
+++ src/imageSDGenFunction.js
@@ -0,0 +1,83 @@
+require('dotenv').config();
+const User = require('../models/user');
+// const { Configuration, OpenAIApi } = require("openai");
+const Discord = require('discord.js');
+const axios = require('axios')
+const fetch = require('node-fetch')
+const fs = require('fs')
+const apiKey = process.env.STABILITY_API_KEY
+
+const config = { headers: { Authorization: `Bearer ${ apiKey }` } }
+
+const engineId = 'stable-diffusion-v1-5'
+const url = `https://api.stability.ai/v1/generation/${ engineId }/text-to-image`
+
+
+
+//below currently works
+// const imageBuffer = await axios.get(url, { responseType: 'arraybuffer' })
+//     .then((response) => {
+//         const buffer = new Buffer.from(response.data)
+//         buffer.name = 'image.png'
+//         return buffer
+//     })
+//     .catch((error) => {
+//         console.error('An error occurred while downloading the file:', error);
+//     });
+
+// console.log(imageBuffer)
+
+async function chatWithAi(args, message, user) {
+    // const initiliazing = "Initializing...";
+    // const username = message.author.username;
+    // const userId = message.author.id;
+    // const guildId = message.guild.id;
+
+
+    try {
+
+        // const prompt = args.join(' ');
+
+
+        const response = await fetch(
+            `https://api.stability.ai/v1/generation/${ engineId }/text-to-image`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                    Authorization: `Bearer ${ apiKey }`,
+                },
+                body: JSON.stringify({
+                    text_prompts: [
+                        {
+                            text: `${ args }`,
+                        },
+                    ],
+                    cfg_scale: 7,
+                    clip_guidance_preset: 'FAST_BLUE',
+                    height: 512,
+                    width: 512,
+                    samples: 1,
+                    steps: 50,
+                }),
+            }
+        )
+
+        if (!response.ok) {
+            throw new Error(`Non-200 response: ${ await response.text() }`);
+        }
+        const responseJSON = await response.json();
+
+
+        return Buffer.from(responseJSON.artifacts[0].base64, 'base64')
+    } catch (e) {
+        console.log(`api error ${ e }`);
+        return e;
+    }
+
+}
+
+
+
+module.exports = { chatWithAi };
