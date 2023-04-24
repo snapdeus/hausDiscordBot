e87b4ebd086acffd15cacde6c972e7c749477e59
snapdeus <snapdeus@gmail.com>
2023-04-24 16:42:10 -0400

    added varysd

---
+++ commands/varysd.js
@@ -0,0 +1,105 @@
+
+const { chatWithAi } = require('../src/varySDGenFunction');
+
+const User = require('../models/user');
+const { v4: uuidv4 } = require('uuid');
+
+
+
+
+
+const talkedRecently = new Set();
+
+function splitMessage(message, maxLength = 2000) {
+    const messageChunks = [];
+    for (let i = 0; i < message.length; i += maxLength) {
+        messageChunks.push(message.slice(i, i + maxLength));
+    }
+    return messageChunks;
+}
+module.exports = {
+    name: 'varysd',
+    description: 'vary image from stable diffusion',
+
+    async execute(client, message, args, member) {
+        const username = message.author.username;
+        const userId = message.author.id;
+        const guildId = message.guild.id;
+
+        // FIND USER IN DB
+        const user = await User.findOne({ userId: userId });
+        if (!user) {
+            const user = new User({
+                username: `${ username }`,
+                userId: `${ userId }`,
+                guildId: `${ guildId }`,
+                xpOverTime: 0,
+                memories: [],
+            });
+            await user.save();
+            return "Initializing...";
+        }
+
+        if (message.attachments.first()?.url === undefined) {
+            message.reply("You must attach a file for this to work. No URL support yet...");
+            return;
+        }
+
+
+        if (message.channel.id !== '975202962173485186' && message.channel.id !== '884434543543726134') {
+            message.channel.send("Please chat with the ai in the 'ai-chat' channel.");
+            return;
+        }
+
+
+
+        if (args.length > 2000) {
+            message.channel.send(`Please limit the length your prompt to 2000 characters.`);
+            return;
+        }
+
+        if (user.xpOverTime > 500) {
+            if (talkedRecently.has(message.author.id)) {
+                message.channel.send("Please wait 5s before issuing another prompt.");
+            } else {
+                try {
+                    const chatBot = client.user.username;
+                    const chatResponse = await chatWithAi(args, message, user, chatBot);
+
+                    if (Buffer.isBuffer(chatResponse.buffer)) {
+                        message.reply({
+                            content: `Here ya go⁣ ${ username } \n Seed: ${ chatResponse.seed } \n Prompt: ${ chatResponse.prompts } \n Negative Prompts: ${ chatResponse.negativePrompts }`, files: [
+                                { attachment: chatResponse.buffer }
+                            ]
+                        });
+                    } else {
+                        console.log(chatResponse);
+                        message.reply({
+                            content: `Hmmm,⁣ ${ username } \n ${ chatResponse }`,
+                        });
+                    }
+
+
+                    user.xpOverTime = user.xpOverTime - 10;
+                    await user.save();
+
+                } catch (e) {
+                    message.channel.send(`ERROR`);
+                    console.log(e);
+                }
+
+                talkedRecently.add(message.author.id);
+                setTimeout(() => {
+                    // Removes the user from the set after a minute
+                    talkedRecently.delete(message.author.id);
+                }, 1000);
+
+            }
+
+
+
+        } else {
+            message.channel.send(`You do not currently have permission to chat with me. Please acquire 🪙500 Haus Coins`);
+        }
+    }
+};
---
+++ src/varySDGenFunction.js
@@ -0,0 +1,125 @@
+require('dotenv').config();
+const User = require('../models/user');
+// const { Configuration, OpenAIApi } = require("openai");
+const Discord = require('discord.js');
+const axios = require('axios');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const apiKey = process.env.STABILITY_API_KEY;
+const path = require('path');
+const config = { headers: { Authorization: `Bearer ${ apiKey }` } };
+const { createCanvas, loadImage, Image } = require('canvas');
+const engineId = 'stable-diffusion-v1-5';
+const FormData = require('form-data');
+
+function removeLetters(inputString) {
+    const stringWithoutLetters = inputString.replace(/[a-zA-Z]/g, '');
+    return parseInt(stringWithoutLetters, 10);
+}
+
+const width = 512;
+const height = 512;
+const canvas = createCanvas(width, height);
+const ctx = canvas.getContext('2d');
+async function loadImageAndDraw(imagePath) {
+    const image = await loadImage(imagePath);
+    ctx.drawImage(image, 0, 0, 512, 512);
+}
+
+
+
+async function chatWithAi(args, message, user) {
+
+    try {
+        const url = message.attachments.first()?.url;
+
+        await loadImageAndDraw(url);
+        const canvasBuffer = canvas.toBuffer('image/png');
+        canvasBuffer.name = 'image.png';
+
+        let mySeed = null;
+        let negativePrompts = [];
+        let prompts = [];
+
+
+
+        args.forEach(arg => {
+            if (arg.startsWith('$')) {
+                if (mySeed) {
+                    throw new Error('More than one seed found. Please provide only one seed.');
+                }
+                mySeed = removeLetters(arg.slice(1)); // Remove the $ sign
+            } else if (arg.startsWith('-')) {
+                negativePrompts.push(arg.slice(1)); // Remove the - prefix
+            } else {
+                prompts.push(arg);
+            }
+        });
+
+        if (prompts.length === 0) {
+            throw new Error('No prompts found. Please provide at least one prompt.');
+        }
+
+        let text_prompts = [
+            {
+                text: `${ prompts }`,
+                weight: 1
+            }
+        ];
+
+        if (negativePrompts.length > 0) {
+            text_prompts.push({
+                text: `${ negativePrompts }`,
+                weight: -1
+            });
+        }
+        console.log(text_prompts[0]['text']);
+
+
+        const formData = new FormData();
+        formData.append('init_image', canvasBuffer);
+        formData.append('init_image_mode', 'IMAGE_STRENGTH');
+        formData.append('image_strength', 0.4);
+        formData.append('text_prompts[0][text]', text_prompts[0]['text']);
+        formData.append('cfg_scale', 7);
+        formData.append('clip_guidance_preset', 'FAST_BLUE');
+        formData.append('samples', 1);
+        formData.append('steps', 50);
+        formData.append('seed', mySeed ? mySeed : 0);
+
+
+
+        const response = await fetch(
+            `https://api.stability.ai/v1/generation/${ engineId }/image-to-image`,
+            {
+                method: 'POST',
+                headers: {
+                    ...formData.getHeaders(),
+                    Accept: 'application/json',
+                    Authorization: `Bearer ${ apiKey }`,
+                },
+                body: formData,
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(`Non-200 response: ${ await response.text() }`);
+        }
+        const responseJSON = await response.json();
+        const chatResponse = {
+            buffer: Buffer.from(responseJSON.artifacts[0].base64, 'base64'),
+            seed: mySeed ? mySeed : 0,
+            prompts: prompts.join(" "),
+            negativePrompts: negativePrompts.length > 0 ? negativePrompts.join(" ") : "N/A"
+        };
+        return chatResponse;
+    } catch (e) {
+        console.log(`api error ${ e }`);
+        return e;
+    }
+
+}
+
+
+
+module.exports = { chatWithAi };
