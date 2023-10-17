ed63ca86e2bfe9f68f37cfffb96c1916a4ce3747
snapdeus <snapdeus@gmail.com>
2023-10-17 15:29:17 -0400

    warnings.js update

--- commands/warnings.js
+++ commands/warnings.js
@@ -1,14 +1,15 @@
 
 const Discord = require('discord.js');
-const db = require('quick.db')
+const { QuickDB } = require("quick.db");
+const db = new QuickDB({ filePath: `./json.sqlite` });
 
 module.exports = {
     name: "warnings",
     description: "Get the warnings of yours or mentioned person",
-    execute: (client, message, args) => {
+    execute: async (client, message, args) => {
         const user = message.mentions.members.first() || message.author;
 
-        let warnings = db.get(`warnings_${ message.guild.id }_${ user.id }`);
+        let warnings = await db.get(`warnings_${ message.guild.id }_${ user.id }`);
 
         if (warnings === null) warnings = 0;
         console.log(warnings);
@@ -16,7 +17,7 @@ module.exports = {
             .setTitle('Warnings')
             .setDescription(`**${ user } has __${ warnings }__ warning(s)**`)
             .setFooter({ text: `${ client.user.username }` })
-            .setThumbnail(client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+            .setThumbnail(client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }));
 
         message.channel.send({ embeds: [warningsem] });
     }
